bd2ce70a7b8a7637e59d61dd4e58e86635188dfd
Elwa36s <24dds08@gmail.com>
2020-12-08 21:42:09 +0300

    feat: add solutions for number part

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -128,8 +128,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const str = value.toString();
+  return str[str.length - 1];
 }
 
 
@@ -144,8 +145,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return +value;
 }
 
 /**
@@ -182,8 +183,9 @@ function getParallelipidedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const numb = num / (10 ** pow);
+  return Math.round(numb) * (10 ** pow);
 }
 
 /**
@@ -203,8 +205,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  const sqrt = Math.ceil(Math.sqrt(n));
+  if (n < 2) return false;
+  if (n === 2) return true;
+  for (let i = 2; i <= sqrt; i += 1) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
@@ -222,8 +230,9 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (Number.isNaN(+value)) return def;
+  return +value;
 }
 
 module.exports = {
