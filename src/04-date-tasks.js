6837ed864cbd51d74a7d6e38314afaa478d668e6
Elwa36s <24dds08@gmail.com>
2020-12-10 15:30:24 +0300

    feat: finish some tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return Date.parse(value);
 }
 
 
@@ -53,8 +53,11 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const currentDate = date;
+  currentDate.setDate(currentDate.getDate() + 59);
+  if (currentDate.getDate() === 1) return false;
+  return true;
 }
 
 
@@ -73,8 +76,20 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const diff = endDate - startDate;
+  let h = Math.floor(diff / 3600000);
+  if (h < 10) h = `0${h}`;
+  let ost = diff % 3600000;
+  let min = Math.floor(ost / 60000);
+  if (min < 10) min = `0${min}`;
+  ost %= 60000;
+  let sec = Math.floor(ost / 1000);
+  if (sec < 10) sec = `0${sec}`;
+  let ms = Math.floor(ost % 1000);
+  if (ms < 100 && ms > 9) ms = `0${ms}`;
+  if (ms < 10) ms = `00${ms}`;
+  return `${h}:${min}:${sec}.${ms}`;
 }
 
 
